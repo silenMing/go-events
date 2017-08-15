ff9b96ed0b29ca2a689c1b44e1754278bc4465da
zhangming <zm7413299@163.com>
2017-08-15 18:49:55 +0800

    addd

--- dispatcher.go
+++ dispatcher.go
@@ -4,3 +4,20 @@ import(
 	"fmt"
 )
 
+type dispatcher struct{
+	listeners map[string] *EventChain
+}
+
+type EventChain struct{
+	chs         [] chan *Event
+	callbacks   [] *EventCallback
+}
+
+type Event struct{
+	eventName string
+	Params    map[string] interface{}
+}
+
+func Create() {
+	
+}
