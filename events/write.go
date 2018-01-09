3cf27f6e1d2a34ee6ecc674e588381a854d244dd
zhangming <zhangming@shopex.com>
2018-01-10 00:43:39 +0800

    read file

---
+++ events/write.go
@@ -0,0 +1,61 @@
+package events
+
+import(
+	io "io/ioutil"
+	json "encoding/json"
+)
+
+type readFile struct{
+
+}
+
+func NewJsonStruct() *readFile{
+	return &readFile{}
+}
+
+func (self *readFile) Load (filename string, v interface{}) {
+
+	data, err := io.ReadFile(filename)
+	
+	if err != nil{
+	
+	return 
+	
+	}
+	
+	datajson := []byte(data)
+	
+	
+	
+	err = json.Unmarshal(datajson, v)
+	
+	if err != nil{
+	
+	return
+	
+	}
+	
+}
+	
+	
+	
+type ValueTestAtmp struct{
+	
+	StringValue string
+	
+	NumericalValue int
+
+	BoolValue bool
+	
+}
+
+type testdata struct {
+
+	ValueTestA ValueTestAtmp
+		
+}
+		
+		
+	
+	
+
